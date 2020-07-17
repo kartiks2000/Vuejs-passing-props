7a8de7eff325cd41ea19617fa11da9afe004c74b
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-07-17 16:23:26 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="kk">
+    <h1>Hello!! I am the parent....</h1>
+    <!-- <button @click="passit">Pass name</button> -->
+    <Child childname="kartik s."></Child>
+  </div>
+</template>
+
+<script>
+import child from './child';
+export default {
+  components:{
+    Child : child
+  },
+  method :{
+    // passit : () => {
+
+    // }
+  }
+}
+</script>
+
+<style scoped>
+  h1{
+    color: red;
+  }
+  div{
+    /* border: 2px solid brown; */
+    margin: 3rem;
+  }
+  .kk{
+    background-color: cornsilk;
+    padding: 3rem;
+  }
+</style>>
+
+</style>
